0689c1e1002b6e5aa0428c35d48e6c393915acfb
Muhammad Dzurrafiq H <dzurrafiq7@gmail.com>
2024-05-06 08:17:33 +0700

    add slider-status

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,60 @@
-  <template>
-    <div id="app" class="container mt-5">
-      <h1>IDShop</h1>
-      <product-list :products="products" :maximum="maximum"></product-list>
-    </div>
-  </template>
+<template>
+  <div id="app" class="container mt-5">
+    <h1>IDShop</h1>
+    <price-slider :sliderStatus="sliderStatus" :maximum.sync="maximum"></price-slider>
+    <product-list
+      :products="products"
+      :maximum="maximum"
+      @add="addItem"
+    ></product-list>
+  </div>
+</template>
 
 <script>
-// import {FontAwesomeIcon} from '@fortawesome/vue-fontawesome'; 
-import ProductList from "./components/ProductList.vue"
+// import {FontAwesomeIcon} from '@fortawesome/vue-fontawesome';
+import PriceSlider from "./components/PriceSlider.vue";
+import ProductList from "./components/ProductList.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   data: function () {
-      return{
-        maximum: 20,
-        products: []
-      }
+    return {
+      maximum: 20,
+      products: [],
+      cart: [],
+      sliderStatus: true
+    };
   },
   components: {
     // FontAwesomeIcon,
-    ProductList
+    ProductList,
+    PriceSlider
   },
-  mounted: function() {
-        fetch('https://hplussport.com/api/products/order/price')
-        .then(response => response.json())
-        .then(data => {
-            this.products = data;
-        });
-  }
-}
-</script>
+  mounted: function () {
+    fetch("https://hplussport.com/api/products/order/price")
+      .then((response) => response.json())
+      .then((data) => {
+        this.products = data;
+      });
+  },
+  methods: {
+    addItem: function (product) {
+      let productIndex;
+      let productExist = this.cart.filter(function (item, index) {
+        if (item.product.id == Number(product.id)) {
+          productIndex = index;
+          return true;
+        } else {
+          return false;
+        }
+      });
 
+      if (productExist.length) {
+        this.cart[productIndex].qty++;
+      } else {
+        this.cart.push({ product: product, qty: 1 });
+      }
+    },
+  },
+};
+</script>
